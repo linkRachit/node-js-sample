5193560b43636ed33a6bcc434939d745fbdf68bd
Rachit Goel <rachit2goel@gmail.com>
2017-03-07 17:32:29 +0530

    variable port on cloud

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = (process.env.PORT || 3000);
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +9,7 @@ const server = http.createServer((req, res) => {
   res.end('Sample Test\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at port:${port}`);
 });
+//${hostname}:${port}
